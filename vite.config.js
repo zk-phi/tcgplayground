ed3ed6d099f31e25f5402b87a49f604bfeb84512
zk-phi <zk-phi@users.noreply.github.com>
2025-01-18 23:55:34 +0900

    Configure vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
+const { resolve } = require("path");
+
 export default defineConfig({
+  root: "src/entrypoints",
   plugins: [preact()],
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        deckmakerdm: "src/entrypoints/index.html",
+      },
+      output: {
+        entryFileNames: "assets/[name]/bundle.js",
+        assetFileNames: (asset) => asset.name === ".css" ? (
+          "assets/[name]/index.css"
+        ) : (
+          "assets/[name]/[hash].[ext]"
+        ),
+      },
+    },
+  },
 });
