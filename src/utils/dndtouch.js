893a42999f347ada03f5ad05b060a1b10e1f036d
zk-phi <zk-phi@users.noreply.github.com>
2025-01-20 02:50:49 +0900

    remove comment

--- src/utils/dndtouch.js
+++ src/utils/dndtouch.js
@@ -2,8 +2,6 @@ import { enableDragDropTouch } from "drag-drop-touch/dist/drag-drop-touch.esm.mi
 
 enableDragDropTouch(undefined, undefined, {
   allowDragScroll: false,
-  /* fire oncontextmenu slightly faster */
-  /* contextMenuDelayMS: 300, */
   /* more permissive to drift before fireing the oncontextmenu event */
   dragThresholdPixels: 32,
 });
