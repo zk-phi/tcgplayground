0b7a9d73fca344c662ae8ae9b1ca88510e640a1f
zk-phi <zk-phi@users.noreply.github.com>
2025-01-20 02:44:41 +0900

    fix?

--- src/utils/dndtouch.js
+++ src/utils/dndtouch.js
@@ -1,8 +1,9 @@
 import { enableDragDropTouch } from "drag-drop-touch/dist/drag-drop-touch.esm.min.js";
 
 enableDragDropTouch(undefined, undefined, {
+  allowDragScroll: false,
   /* fire oncontextmenu slightly faster */
   contextMenuDelayMS: 300,
   /* more permissive to drift before fireing the oncontextmenu event */
-  // dragThresholdPixels: 32,
+  dragThresholdPixels: 32,
 });
