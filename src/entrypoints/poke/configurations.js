507ab21383eaa60ca393e8abda1da4eebf6173ba
zk-phi <zk-phi@users.noreply.github.com>
2025-01-19 20:29:46 +0900

    Add poke demo

---
+++ src/entrypoints/poke/configurations.js
@@ -0,0 +1,176 @@
+import { shuffle as shuffleArray } from "../../utils/array";
+import {
+  stack, gameState, setGameState,
+  move, push, unshift, moveSingle, pushSingle, unshiftSingle,
+  toggleTapped, toggleReversed, toggleFlipped, toggleLaid,
+  shuffle, untapAll,
+} from "../../states/game.js";
+import { dropHandlers, dragHandlers } from "../../states/drag.js";
+import { showList } from "../../states/list.js";
+import { showMenu } from "../../states/menu.js";
+import { showLightbox } from "../../states/lightbox.js";
+
+export const rows = [[
+  { area: "sides", label: "サイド", width: 1 },
+  { area: "field", label: "場" },
+  { area: "graveyard", label: "トラッシュ", width: 1 },
+], [
+  { area: "bench", label: "ベンチ", expandThreshold: 5 },
+  { area: "deck", label: "山", width: 1 },
+], [
+  { area: "hand", label: "手札", expandThreshold: 3 },
+  { area: "exploring", label: "めくられた", optional: true },
+]];
+
+const dragStackHandlers = (src, si, allowEmpty) => dragHandlers(src, si, (e, dest, di) => {
+  if (dest === "graveyard" || dest === "sides") {
+    push(src, si, dest, di ?? 0, true);
+  } else if (dest === "stadium" || di === "deck" || di != null) {
+    showMenu(e, [
+      ["🫳 上に置く", () => push(src, si, dest, di ?? 0, true)],
+      ["🫴 下に入れる", () => unshift(src, si, dest, di ?? 0, true)],
+    ]);
+  } else {
+    move(src, si, dest, {}, (dest !== "hand" && dest !== "exploring"));
+  }
+});
+
+const dragSingleHandlers = (src, si, allowEmpty) => dragHandlers(src, si, (e, dest, di) => {
+  if (dest === "graveyard" || dest === "sides") {
+    pushSingle(src, si, 0, dest, di ?? 0);
+  } else if (dest === "stadium" || di === "deck" || di != null) {
+    showMenu(e, [
+      ["🫳 上に置く", () => pushSingle(src, si, 0, dest, di ?? 0)],
+      ["🫴 下に入れる", () => unshiftSingle(src, si, 0, dest, di ?? 0)],
+    ]);
+  } else {
+    moveSingle(src, si, 0, dest);
+  }
+});
+
+const showListWithContextMenu = (e, area, ix, allowEmpty = false) => {
+  showList(e, area, ix, (j) => ({
+    onClick: e => showMenu(e, [
+      ["🔍 拡大", () => showLightbox(e, gameState.value[area][ix].cards[j])],
+      ["⚔️ バトル場に出す", () => pushSingle(area, ix, j, "field", 0, allowEmpty)],
+      ["🫳 デッキの上に置く", () => pushSingle(area, ix, j, "deck", 0, allowEmpty)],
+      ["🫴 デッキの下に入れる", () => unshiftSingle(area, ix, j, "deck", 0, allowEmpty)],
+      ["🪦 トラッシュに送る", () => pushSingle(area, ix, j, "graveyard", 0, allowEmpty)],
+      ["🪑 ベンチに置く", () => moveSingle(area, ix, j, "bench", allowEmpty)],
+      ["🛡️ サイドに置く", () => pushSingle(area, ix, j, "sides", 0, allowEmpty)],
+      ["🃏 手札に加える", () => moveSingle(area, ix, j, "hand", allowEmpty)],
+    ]),
+    onContextMenu: e => showMenu(e, [
+      ["🔍 拡大", () => showLightbox(e, gameState.value[area][ix].cards[j])],
+      ["⚔️ バトル場に出す", () => pushSingle(area, ix, j, "field", 0, allowEmpty)],
+      ["🫳 デッキの上に置く", () => pushSingle(area, ix, j, "deck", 0, allowEmpty)],
+      ["🫴 デッキの下に入れる", () => unshiftSingle(area, ix, j, "deck", 0, allowEmpty)],
+      ["🪦 トラッシュに送る", () => pushSingle(area, ix, j, "graveyard", 0, allowEmpty)],
+      ["🪑 ベンチに置く", () => moveSingle(area, ix, j, "bench", allowEmpty)],
+      ["🛡️ サイドに置く", () => pushSingle(area, ix, j, "sides", 0, allowEmpty)],
+      ["🃏 手札に加える", () => moveSingle(area, ix, j, "hand", allowEmpty)],
+    ]),
+  }));
+};
+
+export const handlers = {
+  sides: {
+    stack: ix => ({
+      onClick: e => showListWithContextMenu(e, "sides", ix, true),
+      onContextMenu: e => showListWithContextMenu(e, "sides", ix, true),
+      ...dropHandlers("sides", ix),
+      ...dragSingleHandlers("sides", ix, true),
+    }),
+    area: {
+      ...dropHandlers("sides", null),
+    },
+  },
+
+  field: {
+    stack: ix => ({
+      onClick: () => toggleTapped("field", ix),
+      onContextMenu: e => showMenu(e, [
+        ["🔍 拡大", e => showLightbox(e, gameState.value.field[ix].cards[0])],
+        ["👀 重なっているカード", e => showListWithContextMenu(e, "field", ix)],
+      ]),
+      ...dropHandlers("field", ix),
+      ...dragStackHandlers("field", ix),
+    }),
+    area: {
+      ...dropHandlers("field", null),
+    },
+  },
+
+  deck: {
+    stack: ix => ({
+      onClick: e => moveSingle("deck", ix, 0, "exploring", true),
+      onContextMenu: e => showMenu(e, [
+        ["🤏 ボトムから引く", () => moveSingle("deck", ix, -1, "hand", true)],
+        ["♻️ シャッフル", () => shuffle("deck", ix)],
+        ["👀 リスト", e => showListWithContextMenu(e, "deck", ix, true)],
+      ]),
+      ...dropHandlers("deck", ix),
+      ...dragSingleHandlers("deck", ix, true),
+    }),
+    area: {
+      ...dropHandlers("deck", null),
+    },
+  },
+
+  graveyard: {
+    stack: ix => ({
+      onClick: e => showListWithContextMenu(e, "graveyard", ix, true),
+      onContextMenu: e => showListWithContextMenu(e, "graveyard", ix, true),
+      ...dropHandlers("graveyard", ix),
+      ...dragSingleHandlers("graveyard", ix, true),
+    }),
+    area: {
+      ...dropHandlers("graveyard", null),
+    },
+  },
+
+  bench: {
+    stack: ix => ({
+      onClick: e => toggleTapped("bench", ix),
+      onContextMenu: e => showMenu(e, [
+        ["🔍 拡大", e => showLightbox(e, gameState.value.bench[ix].cards[0])],
+        ["👀 重なっているカード", e => showListWithContextMenu(e, "bench", ix)],
+      ]),
+      ...dropHandlers("bench", ix),
+      ...dragStackHandlers("bench", ix),
+    }),
+    area: {
+      ...dropHandlers("bench", null),
+    },
+  },
+
+  hand: {
+    stack: ix => ({
+      onClick: e => preventDefault(e),
+      onContextMenu: e => showMenu(e, [
+        ["🔍 拡大", e => showLightbox(e, gameState.value.hand[ix].cards[0])],
+        ["👀 重なっているカード", e => showListWithContextMenu(e, "hand", ix)],
+      ]),
+      ...dropHandlers("hand", ix),
+      ...dragStackHandlers("hand", ix),
+    }),
+    area: {
+      ...dropHandlers("hand", null),
+    },
+  },
+
+  exploring: {
+    stack: ix => ({
+      onClick: e => preventDefault(e),
+      onContextMenu: e => showMenu(e, [
+        ["🔍 拡大", e => showLightbox(e, gameState.value.exploring[ix].cards[0])],
+        ["👀 重なっているカード", e => showListWithContextMenu(e, "hand", ix)],
+      ]),
+      ...dropHandlers("exploring", ix),
+      ...dragStackHandlers("exploring", ix),
+    }),
+    area: {
+      ...dropHandlers("exploring", null),
+    },
+  },
+};
---
+++ src/entrypoints/poke/demo/main.jsx
@@ -0,0 +1,118 @@
+import { render } from "preact";
+import { useEffect } from "preact/hooks";
+import * as configurations from "../configurations.js";
+
+import { gameState,  stack, setGameState, untapAll } from "../../../states/game.js";
+import { shuffle as shuffleArray } from "../../../utils/array.js";
+
+import { Playground } from "../../../Playground";
+import { FloatingButtons } from "../../../components/FloatingButtons";
+import { Button } from "../../../components/Button";
+
+import "../../../styles.min.css";
+import "./style.css";
+import { enableDragDropTouch } from "drag-drop-touch/dist/drag-drop-touch.esm.min.js";
+
+enableDragDropTouch(undefined, undefined, {
+  contextMenuDelayMS: 400,
+});
+
+const sampleDeck = [
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/ENE/034670_E_KIHONCHOUENERUGI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037256_P_MYUUTSUMYUUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037256_P_MYUUTSUMYUUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037256_P_MYUUTSUMYUUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037256_P_MYUUTSUMYUUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037257_P_OROTTOYONOWARUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037257_P_OROTTOYONOWARUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM9/035982_P_GENGAMIMIKKYUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12/037137_P_MEGAMIMIROPPUPURINGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM11/036781_P_RATEIOSUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM10b/036689_P_AGOYONGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM11/036816_P_KAIRYUGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037252_P_DEDENNEGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037252_P_DEDENNEGX.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037265_P_MAIKA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037265_P_MAIKA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SMM/036846_P_KARAMANERO.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SMM/036846_P_KARAMANERO.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SMM/036846_P_KARAMANERO.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM7a/035204_P_METAMON.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM8a/035422_P_ABUSORU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041117_T_KUIKKUBORU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041117_T_KUIKKUBORU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041117_T_KUIKKUBORU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041117_T_KUIKKUBORU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037338_T_MISUTERITOREJA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037338_T_MISUTERITOREJA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037338_T_MISUTERITOREJA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037338_T_MISUTERITOREJA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037331_T_PURESHASUBORU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12a/037331_T_PURESHASUBORU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/S11a/042022_T_POKEMONIREKAE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/S11a/042022_T_POKEMONIREKAE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/S11a/042022_T_POKEMONIREKAE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/S11a/042022_T_POKEMONIREKAE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SMP/035397_T_ENERUGISUPINA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SMP/035397_T_ENERUGISUPINA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SC2/038902_T_RISETTOSUTANPU.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM11a/036956_T_GURETOKYACCHA.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041125_T_OOKINAOMAMORI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041125_T_OOKINAOMAMORI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041126_T_FUUSEN.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041126_T_FUUSEN.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SA/037595_T_HAKASENOKENKYUUMAGUNORIAHAKASE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SA/037595_T_HAKASENOKENKYUUMAGUNORIAHAKASE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SA/037595_T_HAKASENOKENKYUUMAGUNORIAHAKASE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SA/037595_T_HAKASENOKENKYUUMAGUNORIAHAKASE.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041131_T_MARII.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041131_T_MARII.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041131_T_MARII.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SK/041131_T_MARII.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12/037155_T_KONTONNOUNERI.jpg",
+  "https://www.pokemon-card.com/assets/images/card_images/large/SM12/037155_T_KONTONNOUNERI.jpg",
+];
+
+const initialize = () => {
+  const deck = shuffleArray(sampleDeck);
+  setGameState({
+    sides: [stack({ cards: deck.splice(0, 6), flipped: true })],
+    hand: deck.splice(0, 7).map(src => stack({ cards: [src] })),
+    deck: [stack({ cards: deck, flipped: true })],
+    field: [],
+    graveyard: [stack({ cards: [] })],
+    bench: [],
+    exploring: [],
+  });
+  console.log(gameState.value);
+};
+
+const App = () => {
+  useEffect(() => initialize(), []);
+  return (
+    <>
+      <FloatingButtons>
+        <Button onClick={() => untapAll(["field", "lands"])}>
+          アンタップ
+        </Button>
+        <Button onClick={initialize}>
+          リセット
+        </Button>
+      </FloatingButtons>
+      <Playground initialize={initialize} {...configurations} />
+    </>
+  );
+};
+
+const div = document.getElementById("dmplayground");
+document.body.append(div);
+
+render(<App />, div)
