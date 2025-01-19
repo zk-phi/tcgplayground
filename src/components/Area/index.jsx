44fec185d4887630aa8c1b0982281ef6d7e140d7
zk-phi <zk-phi@users.noreply.github.com>
2025-01-19 18:29:05 +0900

    remove unused arg

--- src/components/Area/index.jsx
+++ src/components/Area/index.jsx
@@ -1,4 +1,4 @@
-export const Area = ({ area, label, children, width, nogrow, isTargetted, ...props }) => {
+export const Area = ({ label, children, width, nogrow, isTargetted, ...props }) => {
   const style = {
     width: width == null ? undefined : (
       `calc(var(--dmpg-card-width) * ${width} + var(--dmpg-card-gap) * ${width - 1})`
